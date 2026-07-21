cd0178d8469175b884e46b802c1168c2329c8927
Zachary Giles <zachary.giles@cnu.edu>
2026-07-21 16:11:58 -0400

    Practice Problems

---
+++ CS50P Practice/Einstein.py
@@ -0,0 +1,5 @@
+def energy(m: int) -> int:
+    return m * 300000000 ** 2
+
+if __name__ == '__main__':
+    print(energy(int(input("m: "))))
--- CS50P Practice/Faces.py
+++ CS50P Practice/Faces.py
@@ -1,1 +1,8 @@
+def convert(a: str) -> str:
+    a = a.replace(':)', '🙂')
+    a = a.replace(':(', '🙁')
+    return a
 
+if __name__ == '__main__':
+    b = input("Input your text: ")
+    print(convert(b))
