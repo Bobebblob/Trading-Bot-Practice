1ccd1fda306ef9678baedf1ef5d83dc48b0c986d
Zachary Giles <zachary.giles@cnu.edu>
2026-08-11 00:06:48 -0400

    Rough Draft for Info Prints and Moving Average

--- Functional Practice/Stock Imager.py
+++ Functional Practice/Stock Imager.py
@@ -8,14 +8,6 @@ import yfinance as yf
 def load(name, time):
     return yf.download(name, period=time)
 
-#display summary
-#plot price
-#plot volume
-#calculate returns
-#find biggest gain
-#find biggest loss
-#plot moving average
-
 if __name__ == '__main__':
     stock = input('Input the Ticker you would like to visualize: ').upper()
     timeperiod = input(f'What length of time would you like to visualize?\n'
@@ -27,9 +19,10 @@ if __name__ == '__main__':
     dfhigh = df['High']
     dflow = df['Low']
     dfvol = df['Volume']
+    mavg = (dfclose + dfopen)/2
 
     figure, axes = plt.subplots(3,2)
-    figure.delaxes(axes[2, 1])
+    #figure.delaxes(axes[2, 1])
 
     #print(dfdates)
 
@@ -39,6 +32,7 @@ if __name__ == '__main__':
     axes[1, 0].plot(dfdates, dfhigh)
     axes[1, 1].plot(dfdates, dflow)
     axes[2, 0].plot(dfdates, dfvol)
+    axes[2, 1].plot(dfdates, mavg)
     #axes.ylabel('Points')
     #axes.xlabel('Date of Close')
     #plt.title(f"{stock} Closing Prices")
@@ -46,4 +40,12 @@ if __name__ == '__main__':
     plt.show()
     print(f'Number of Trading Days: {dfdates.size}\n'
           f'Starting Date: {dfdates[0].tz_localize(None)}\n'
-          f'End Date: {dfdates[-1].tz_localize(None)}\n')
+          f'End Date: {dfdates[-1].tz_localize(None)}\n\n'
+          'Closing Data: \n'
+          f'Highest Closing Price: {dfclose.max(numeric_only=True)}\n'
+          f'Lowest Closing Price: {dfclose.min(numeric_only=True)}\n'
+          f'Average Closing Price: {dfclose.mean(numeric_only=True)}\n\n'
+          'Gain & Loss Data: \n'
+          f'Largest Day Gain: {(dfclose-dfopen).max(numeric_only=True)} points\n'
+          f'Largest Day Loss: {(dfclose-dfopen).min(numeric_only=True)*(-1)} points\n\n'
+          )
